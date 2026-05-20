20e3462a95a98a44193e433069753ecd70ed97c8
k-rsouza <k.souza@edu.pucrs.br>
2026-05-20 16:42:53 -0300

    updating hero.tsx, page.tsx and layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
 import "./globals.css";
+import { Red_Hat_Display } from "next/font/google";
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export default function RootLayout({
   children,
@@ -7,10 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={'antialiased'}
+      lang="pt-BR"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`${redHatDisplay.className} bg-background-primary text-content-body antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-import Image from "next/image";
+import Hero from "./components/landing-page/hero";
 
-export default function Home() {return <div> Hello World </div>}
+export default function Home() {
+    return <div className="max-w-7x1 mx-auto border"> 
+    <div className="text-white"> HELLO</div>
+    <Hero />
+    
+    {/*<Header /> 
+    <VideoExplanation />
+    <Pricing />
+    <FAQ /> */}
+
+    </div>
+}
